a52a90a2aacb4cd6a8776180fe7a66a123616c5e
Robert Corty <rcorty@gmail.com>
2015-10-27 11:20:58 -0400

    put function for test1.R into test1.R
    this was necessary to pass CRAN checks
    
    Signed-off-by: Robert Corty <rcorty@gmail.com>

--- tests/test1.R
+++ tests/test1.R
@@ -1,14 +1,33 @@
 library(dglm)
-library(CortyKit)
 
 n <- 400
+
+TestFunc <- function(mean.formula, var.formula, model.df) {
+  
+  my.glm <- glm(formula = mean.formula,
+                data = model.df)
+  
+  x <- predict(my.glm, se.fit = TRUE)
+  
+  my.dglm <- dglm(formula = mean.formula,
+                  dformula = var.formula,
+                  data = model.df)
+  
+  y <- predict(my.dglm, se.fit = TRUE)
+  
+  z <- predict(my.dglm$dispersion.fit, se.fit = TRUE)
+  
+  return(list(glm = x, dglm.mean = y, dglm.var = z))
+}
+
+
+
 
 a <- runif(n)
 b <- runif(n)
 c <- runif(n)
 d <- runif(n)
 
-# betas <- c(runif(n = 3, min = 10, max = 20), 2, 1, 1)
 betas <- c(runif(n = 3, min = 10, max = 20), runif(n = 3, min = 1, max = 2))
 
 mu <- betas[1] + a*betas[2] + b*betas[3]
@@ -24,33 +43,6 @@ l <- TestFunc(mean.formula = mean.formula, var.formula = var.formula, model.df =
 
 par(mfrow = c(1, 2))
 plot(mu, l$dglm.mean$fit); abline(0, 1)
-legend(x = 'topleft', legend = paste(c('intercept  ', '\t\t\t\t\ta', '\t\t\t\t\tb'), round(betas[1:3], 1)))
+legend(x = 'topleft', legend = paste(c('intercept  ', '           a', '          b'), round(betas[1:3], 1)))
 plot(sd, exp(l$dglm.var$fit/l$dglm.var$residual.scale)); abline(0, 1)
-legend(x = 'topleft', legend = paste(c('intercept  ', '\t\t\t\t\tc', '\t\t\t\t\td'), round(betas[4:6], 2)))
-
-# pdf(file = 'heterosked_fit_all_gray.pdf', width = 15, height = 8)
-# plot(x = my.fit$linear.predictors, y = y, 
-#      main = 'DGLM illustration',
-#      xlab = 'Predicted response',
-#      ylab = 'True response',
-#      col = gray(0.5, 0.8),
-#      pch = 19, cex = 1.5)
-# dev.off()
-# 
-# pdf(file = 'heterosked_fit_dark_high_var.pdf', width = 15, height = 8)
-# plot(x = my.fit$linear.predictors, y = y, 
-#      main = 'DGLM illustration',
-#      xlab = 'Predicted response',
-#      ylab = 'True response',
-#      col = gray(1 - rescale(sd), 0.8),
-#      pch = 19, cex = 1.5)
-# dev.off()
-# 
-# pdf(file = 'heterosked_fit_dark_low_var.pdf', width = 15, height = 8)
-# plot(x = my.fit$linear.predictors, y = y, 
-#      main = 'DGLM illustration',
-#      xlab = 'Predicted response',
-#      ylab = 'True response',
-#      col = gray(rescale(sd), 0.8),
-#      pch = 19, cex = 1.5)
-# dev.off()
+legend(x = 'topleft', legend = paste(c('intercept  ', '           c', '           d'), round(betas[4:6], 2)))
